a2cc127010e2e689d2ca92b5b99f81fe1476d656
Josef Friedrich <jf@josef-friedrich.de>
2015-02-06 16:07:51 +0100

    Clean up radius_library.sh

--- radius_library.sh
+++ radius_library.sh
@@ -27,40 +27,13 @@ _mysql_silent() {
 }
 
 _password_generator() {
-  # -a algorithm
-  #        use algorithm for password generation.
-  #        0 - (default) pronounceable password generation
-  #        1 - random character password generation
-  #
-  # -n num_of_pass
-  #        generate num_of_pass number of passwords. Default is 6.
-  #
-  # -m min_pass_len
-  #        generate  password  with  minimum  length   min_pass_len.    If
-  #        min_pass_len  >  max_pass_len then max_pass_len = min_pass_len.
-  #        Default minimum password length is 8.
-  #
-  # -x max_pass_len
-  #        generate  password  with  maximum  length   max_pass_len.    If
-  #        min_pass_len  >  max_pass_len then max_pass_len = min_pass_len.
-  #        Default maximum password length is 10.
-  #
-  # -M mode
-  #        Use symbolsets specified with  mode  for  password  generation.
-  #        mode  is  a text string consisting of characters S, s, N, n, C,
-  #        c, L, l. Where:
-  #
-  #        L      generator must use small letters symbol  set  for  every
-  #               generated  password  (always  present  if  pronounceable
-  #               password generation algorithm is used).
+  local WEAK="-m 6 -x 6 -M L"
+  local STRONG="-m 8 -x 8 -M CLN"
 
-  WEAK="-m 6 -x 6 -M L"
-  STRONG="-m 8 -x 8 -M CLN"
-
-  APPEND="$STRONG"
+  local APPEND="$STRONG"
 
   if [ "$1" == "weak" ]; then
-    APPEND="$WEAK"
+    local APPEND="$WEAK"
   fi
 
   apg -a 1 -n 1 $APPEND
@@ -74,7 +47,7 @@ function _label {
     local SUGGESTION_FORMATED=" [$SUGGESTION]"
   fi
 
-  echo "$LABEL$SUGGESTION_FORMATED: "
+  echo -n "$LABEL$SUGGESTION_FORMATED: "
 }
 
 function _prompt {
